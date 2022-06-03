999d901b704e4470a7bd302a675da9f80171924d
Abrar Reza <55370306+Abrar-Reza@users.noreply.github.com>
2022-06-03 17:43:52 -0400

    strapi to cloud

--- config/database.js
+++ config/database.js
@@ -1,3 +1,17 @@
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: env('DATABASE_HOST', '127.0.0.1'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'strapi'),
+      user: env('DATABASE_USERNAME', 'strapi'),
+      password: env('DATABASE_PASSWORD', 'strapi'),
+      ssl: env.bool('DATABASE_SSL', false), // For self-signed certificates
+    },
+    debug: false,
+  },
+});
 // const path = require('path');
 
 // module.exports = ({ env }) => ({
@@ -9,17 +23,3 @@
 //     useNullAsDefault: true,
 //   },
 // });
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'postgres',
-    connection: {
-      host: env('DATABASE_HOST'),
-      port: env.int('DATABASE_PORT'),
-      database: env('DATABASE_NAME'),
-      user: env('DATABASE_USERNAME'),
-      password: env('DATABASE_PASSWORD'),
-      ssl: env.bool('DATABASE_SSL'), 
-    },
-    debug: false,
-  },
-});
