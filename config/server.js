846b136c7649b21e14d04043e70918d391179d99
Abrar Reza <55370306+Abrar-Reza@users.noreply.github.com>
2022-06-03 21:40:39 -0400

    idk

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: "https://strapi-todo-backend.azurewebsites.net",
+  url: "https://todo-strapi-backend.azurewebsites.net",
   app: {
     keys: env.array('APP_KEYS'),
   },
